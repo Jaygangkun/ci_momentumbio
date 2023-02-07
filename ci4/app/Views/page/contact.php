f862c9d778c488b6f83a35c547c769ef57946587
JayGangkun <jaygangkun@hotmail.com>
2023-02-07 11:42:06 -0500

    no message

--- ci4/app/Views/page/contact.php
+++ ci4/app/Views/page/contact.php
@@ -4,7 +4,9 @@
             <h1 class="fs-32">Contact</h1>
             <div class="contact-wrap">
                 <div class="contact-wrap-left">
-                    <iframe class="embeded-map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24195.595551644004!2d-73.99982608701175!3d40.708121475222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1675310560715!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div class="embeded-map-wrap">
+                        <iframe class="embeded-map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d24195.595551644004!2d-73.99982608701175!3d40.708121475222!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1675310560715!5m2!1sen!2sus" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
                 </div>
                 <div class="contact-wrap-right">
                     <div class="contact-form">

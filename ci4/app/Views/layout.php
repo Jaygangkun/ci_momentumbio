bf6e1876059158383c605de583bc6ea7af41ca2d
JayGangkun <jaygangkun@hotmail.com>
2023-04-05 22:19:31 -0400

    add google tag

--- ci4/app/Views/layout.php
+++ ci4/app/Views/layout.php
@@ -9,6 +9,12 @@
 
 	<link rel="icon" type="image/x-icon" href="<?= base_url('/assets/img/m-logo-black.png')?>">
 
+  <!-- Google tag (gtag.js) --> 
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-RQB54SR123"> </script> 
+  <script>   
+  window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());   gtag('config', 'G-RQB54SR123'); 
+  </script>
+
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
